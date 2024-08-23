bf87c0c465ad9f52cc78c9c17715f20c44e94f15
Krzysztof Bruszewski <krzysztof.bruszewski@gmail.com>
2024-08-23 22:40:59 +0200

    Update Ubuntu Startship prompt.sh

--- Ubuntu Startship prompt.sh
+++ Ubuntu Startship prompt.sh
@@ -5,26 +5,8 @@ curl -sS https://starship.rs/install.sh | sh
 # To get started configuring starship, create the following file: ~/.config/starship.toml.
 mkdir -p ~/.config && touch ~/.config/starship.toml
 
-# All configuration for starship is done in this TOML (opens new window)file:
-[character]
-success_symbol = '[\$](bold green)'
-
-[username]
-style_user = '#a6aaf1 bold'
-style_root = 'white bold'
-format = '[$user]($style)'
-disabled = false
-show_always = true
-
-[hostname]
-ssh_only = false
-format = '[@](white bold)[$hostname](#50fa7b bold)'
-disabled = false
-
-[directory]
-style = '#00a5ff bold'
-truncation_length = 0
-truncate_to_repo = false
+# A Nerd Font installed and enabled in your terminal (the example uses Fira Code Nerd Font)
+starship preset nerd-font-symbols -o ~/.config/starship.toml
 
 # Bash
 # Add the following to the end of ~/.bashrc:
