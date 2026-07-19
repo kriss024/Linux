a5c4a4b868515201025289035308d22460814f23
Krzysztof Bruszewski <krzysztof.bruszewski@gmail.com>
2026-07-19 18:11:02 +0200

    Fix formatting in Ubuntu Docker.sh

--- Ubuntu Docker.sh
+++ Ubuntu Docker.sh
@@ -14,7 +14,6 @@ echo \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 sudo apt-get update
-
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 # Start and Automate Docker
